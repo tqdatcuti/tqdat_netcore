c405259994db4325b7c49931501c67c4acbf7ec5
Tqdatt <dat1712006@gmail.com>
2026-09-26 13:34:57 +0700

    ok

--- TqdLesson10/Program.cs
+++ TqdLesson10/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BookStoreContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("BookStoreConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookStoreConnection")));
 
 var app = builder.Build();
 
